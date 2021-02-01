821ef26a17af00dfed2dc0d29cebc7c31b3589bd
Clinton Davelaar <Davelaarprogramming@gmail.com>
2021-02-01 14:50:14 -0600

    Initial Commit

---
+++ ComplexGroupsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComplexGroupsSwiftUI
+//
+//  Created by Davelaar,Clinton B on 2/1/21.
+//  Copyright © 2021 Davelaar,Clinton B. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
